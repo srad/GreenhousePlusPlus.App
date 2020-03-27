ff733960de0fe02e2795c402abf3b3b219838aee
srad <ssedighi@posteo.de>
2020-03-27 01:41:21 +0100

    Filter value sliders. Improved UI.

--- src/components/ImageFrame.vue
+++ src/components/ImageFrame.vue
@@ -1,6 +1,6 @@
 <template>
   <b-card-group deck>
-    <b-card class="shadow-sm bg-dark" footer-tag="footer" body-bg-variant="dark" footer-bg-variant="primary" border-variant="primary" body-class="p-0 text-center" footer-class="p-1 pl-2 pr-2">
+    <b-card class="bg-dark" footer-tag="footer" body-bg-variant="dark" :footer-bg-variant="variant" border-variant="dark" body-class="p-0 text-center" footer-class="p-1 pl-2 pr-2">
       <b-card-img :style="imgStyle" :class="imgClass" :src="src" @click="$emit('select', obj)"/>
       <template v-slot:footer>
         <span class="text-light">{{caption}}</span>
@@ -18,10 +18,11 @@ export default {
   props: {
     src: String,
     caption: String,
-    obj: String,
+    obj: Object,
     destroyButton: Boolean,
     imgClass: String,
     imgStyle: String,
+    variant: String,
   },
   methods: {
     destroy(obj) {
--- src/views/ImageViewer.vue
+++ src/views/ImageViewer.vue
@@ -1,61 +1,110 @@
 <template>
   <div>
-    <b-row v-show="working" class="position-fixed bg-dark overlay">
-      <b-col class="text-center">
-        <b-spinner class="mb-3" variant="success" style="width: 4rem; height: 4rem;"/>
-        <h2 class="text-light">Working...</h2>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <b-form-group>
-          <b-form-file
-              id="file-large"
-              :state="Boolean(file)"
-              v-model="file"
-              accept="image/jpeg, image/png"
-              placeholder="Choose a file or drop it here..."
-              drop-placeholder="Drop file here..."
-              size="md"/>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col cols="6">
-        <b-row>
-          <b-col>
-            <b-row class="mb-3">
+  <b-row>
+    <!-- Content -->
+    <b-col>
+      <b-row v-show="working" class="position-fixed bg-dark overlay">
+        <b-col class="text-center">
+          <b-spinner class="mb-3" variant="success" style="width: 4rem; height: 4rem;"/>
+          <h2 class="text-light">Working...</h2>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <b-card class="shadow-sm" header-class="pl-2 pr-2 pt-1 pb-1" header="Upload Image" no-body bg-variant="dark" header-bg-variant="primary" header-text-variant="white">
+            <b-form-group>
+              <b-form-file
+                  id="file-large"
+                  :state="Boolean(file)"
+                  v-model="file"
+                  accept="image/jpeg, image/png"
+                  placeholder="Choose a file or drop it here..."
+                  drop-placeholder="Drop file here..."
+                  size="md"/>
+            </b-form-group>
+          </b-card>
+        </b-col>
+      </b-row>
+
+      <b-row class="mb-3">
+        <b-col>
+          <b-row class="mb-3">
+            <b-col>
+              <b-card class="shadow-sm" body-class="p-1" header-class="pl-2 pr-2 pt-1 pb-1" header="Pipeline" border-variant="success" bg-variant="dark" header-bg-variant="success" header-text-variant="white">
+                <b-row>
+                  <b-col v-for="(file, index) in row1 " :key="index">
+                    <image-frame variant="success" :src="file.image.src + '?' + file.image.bump" :caption="file.caption" :obj="file" @select="show(file.image.src)"/>
+                    <b-row>
+                      <b-col sm="8">
+                        <b-form-input class="m-1" v-model="filterValues[file.filter.key]" @mouseup="process({name: openFile.name})" type="range" :min="file.filter.min" :max="file.filter.max" :step="file.filter.steps"/>
+                      </b-col>
+                      <b-col sm="2">
+                        <span class="badge badge-success">{{filterValues[file.filter.key]}}</span>
+                      </b-col>
+                    </b-row>
+                  </b-col>
+                </b-row>
+              </b-card>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <b-card class="shadow-sm" body-class="p-1" header-class="pl-2 pr-2 pt-1 pb-1" header="Segmentation" border-variant="info" bg-variant="dark" header-bg-variant="info" header-text-variant="white">
+                <b-row>
+                  <b-col v-for="(file, index) in row2" :key="index">
+                    <image-frame variant="info" :src="file.src" caption="Filtered Non-leaf" :obj="file" @select="show(file.src)"/>
+                  </b-col>
+                </b-row>
+              </b-card>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <b-card class="shadow-sm" body-class="p-1" header-class="pl-2 pr-2 pt-1 pb-1" header="Histograms" border-variant="danger" bg-variant="dark" header-bg-variant="danger" header-text-variant="white">
+            <b-row>
               <b-col v-for="(h, index) in hist" :key="index">
-                <image-frame :src="h" caption="Histogram Red" :obj="h" @select="show(h)"/>
+                <image-frame variant="danger" :src="h.src" caption="Histogram Red" :obj="h" @select="show(h.src)"/>
               </b-col>
             </b-row>
-            <b-row class="mb-3">
-              <b-col v-for="(file, index) in row1 " :key="index">
-                <image-frame :src="file.name" :caption="file.caption" :obj="file.name" @select="show(file.name)"/>
-              </b-col>
-            </b-row>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col v-for="(file, index) in row2" :key="index">
-            <image-frame :src="file" caption="Filtered Non-leaf" :obj="file" @select="show(file)"/>
-          </b-col>
-        </b-row>
-      </b-col>
-      <b-col cols="4">
-        <image-frame :src="files.original" img-style="max-height:300px; width:auto;" caption="Original" :obj="files.original"/>
-      </b-col>
-      <b-col>
-        <div class="p-0" style="max-height: 1000px; overflow-x: hidden; overflow-y: scroll">
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-col>
+    <!-- Content end -->
+
+    <!-- Sidebar -->
+    <b-col cols="2">
+      <b-card class="shadow-sm" header-class="p-2" header="Available images" body-class="p-1" bg-variant="dark" header-bg-variant="primary" border-variant="primary" header-text-variant="white">
+        <div>
           <image-frame
-              class="mb-2"
-              v-for="image in images" :key="image.name"
+              variant="primary"
+              :class="{'mt-1' : index > 0}"
+              v-for="(image, index) in images" :key="image.name"
               :src="image.path"
               :destroy-button="true"
-              :obj="image.name"
+              :obj="image"
               v-on:destroy="destroy"
               v-on:select="process"/>
         </div>
+      </b-card>
+    </b-col>
+    <!-- Sidebar end -->
+
+  </b-row>
+    <b-row>
+      <b-col>
+        <b-card class="mt-3 shadow-sm" body-class="p-1" header-class="pl-2 pr-2 pt-1 pb-1" header="Whole Pipeline (one image)" border-variant="secondary" bg-variant="dark" header-bg-variant="secondary" header-text-variant="white">
+          <b-row>
+            <b-col>
+              <b-img fluid-grow :src="files.pipeline.src + '?' + files.pipeline.bump"/>
+            </b-col>
+          </b-row>
+        </b-card>
       </b-col>
     </b-row>
 
@@ -80,19 +129,27 @@ export default {
       view: "",
       showModal: false,
       working: true,
+      openFile: {name},
       files: {
-        "original": "",
-        "hist-red": "",
-        "hist-green": "",
-        "hist-blue": "",
-        "pass": "",
-        "leaf": "",
-        "tip": "",
-        "red": "",
-        "green": "",
-        "edge": "",
-        "earth": "",
-        "blur": "",
+        "pipeline": {src: "", bump: ""},
+        "original": {src: "", bump: ""},
+        "hist-red": {src: "", bump: ""},
+        "hist-green": {src: "", bump: ""},
+        "hist-blue": {src: "", bump: ""},
+        "pass": {src: "", bump: ""},
+        "leaf": {src: "", bump: ""},
+        "tip": {src: "", bump: ""},
+        "red": {src: "", bump: ""},
+        "green": {src: "", bump: ""},
+        "edge": {src: "", bump: ""},
+        "earth": {src: "", bump: ""},
+        "blur": {src: "", bump: ""},
+      },
+      filterValues: {
+        thetaTheshold: 0.83,
+        whiteThreshold: 25,
+        blurRounds: 40,
+        scanlineInterpolationWidth: 0,
       },
       file: null,
       images: [],
@@ -108,10 +165,10 @@ export default {
     },
     row1() {
       return [
-        {name: this.files.edge, caption: "Sobel Operator"},
-        {name: this.files.blur, caption: "Vertical Blur"},
-        {name: this.files.pass, caption: "High Pass"},
-        {name: this.files.tip, caption: "Top Location"},
+        {image: this.files.edge, caption: "Sobel Operator", filter: {min: 0.0, max: 1.0, steps: 0.1, title: "Edge-Theta", "key": "thetaTheshold"}},
+        {image: this.files.blur, caption: "Vertical Blur", filter: {min: 0, max: 200, steps: 1, title: "V-Blur", "key": "blurRounds"}},
+        {image: this.files.pass, caption: "High Pass", filter: {min: 0, max: 255, steps: 1, title: "High pass filter", "key": "whiteThreshold"}},
+        {image: this.files.tip, caption: "Top Location", filter: {min: 0, max: 50, steps: 1, title: "Tolerance Window", "key": "scanlineInterpolationWidth"}},
       ];
     },
     row2() {
@@ -144,7 +201,7 @@ export default {
       }).catch(error => {
         alert(error.response.data.title);
         this.working = false;
-      })
+      });
     },
   },
   methods: {
@@ -152,21 +209,30 @@ export default {
       this.view = url;
       this.showModal = true;
     },
-    process(name) {
+    process(file) {
       this.working = true;
-      this.api.put("/images/" + name)
-        .then(response => {
-          this.loadFiles(response.data);
-          this.working = false;
-        });
+      this.openFile = file;
+      this.api.put("/images", {
+        file: file.name,
+        thetaTheshold: Number(this.filterValues.thetaTheshold),
+        whiteThreshold: Number(this.filterValues.whiteThreshold),
+        blurRounds: Number(this.filterValues.blurRounds),
+        scanlineInterpolationWidth: Number(this.filterValues.scanlineInterpolationWidth),
+      }).then(response => {
+        this.loadFiles(response.data.files);
+        this.working = false;
+      }).catch(error => {
+        alert(error.response.data.errors.join("\n"));
+        this.working = false;
+      });
     },
-    destroy(name) {
+    destroy(file) {
       this.working = true;
-      this.api.delete("/images/" + name)
+      this.api.delete("/images/" + file.name)
         .then(() => {
-          const image = this.images.filter(image => image.name === name)[0];
+          const image = this.images.filter(image => image.name === file.name)[0];
           this.images.splice(this.images.indexOf(image), 1);
-          if (this.files.original.indexOf(name) !== -1) {
+          if (this.files.original.src.indexOf(file.name) !== -1) {
             this.reset();
           }
           this.working = false;
@@ -174,7 +240,7 @@ export default {
     },
     loadFiles(files) {
       files.forEach(file => {
-        this.files[file.element] = API_URL + file.path;
+        this.files[file.element] = {src: API_URL + file.path, bump: new Date().getTime() };
       });
     },
     thumblist() {
@@ -187,14 +253,14 @@ export default {
           response.data.forEach(file => {
             file.path = API_URL + file.path;
             this.images.push(file);
-          })
+          });
           this.working = false;
         }).catch(err => console.log(err));
     },
     reset() {
       Object.keys(this.files)
         .forEach(file => {
-          this.files[file] = API_URL + "/static/leaf.png";
+          this.files[file].src = API_URL + "/static/leaf.png";
         });
     },
   },
